1f18ac8143f6c0e664dff4b2ee1ff79861dc419c
Esteban <estebancanoc@gmail.com>
2023-05-08 17:29:20 -0500

    Agregado ClienteController - Agregada funcionalidad @PostMapping CrearCliente();

---
+++ src/main/java/com/esteban/mensajeria/controller/ClienteController.java
@@ -0,0 +1,24 @@
+package com.esteban.mensajeria.controller;
+
+import com.esteban.mensajeria.dto.ClienteDTO;
+import com.esteban.mensajeria.model.Cliente;
+import com.esteban.mensajeria.service.ClienteService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/")
+public class ClienteController {
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping
+    public Cliente crearCliente(@RequestBody ClienteDTO clienteDTO){
+        return this.clienteService.crearCliente(clienteDTO);
+    }
+}
