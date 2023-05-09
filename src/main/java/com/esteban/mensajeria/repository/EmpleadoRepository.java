aed1a88914bde1c2f3317985fcbf9c2132fe4f61
Esteban <estebancanoc@gmail.com>
2023-05-09 16:16:47 -0500

    Agregado EmpleadoRepository para la conexion por medio de JPA

---
+++ src/main/java/com/esteban/mensajeria/repository/EmpleadoRepository.java
@@ -0,0 +1,9 @@
+package com.esteban.mensajeria.repository;
+
+import com.esteban.mensajeria.model.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpleadoRepository extends JpaRepository<Empleado, Integer> {
+}
