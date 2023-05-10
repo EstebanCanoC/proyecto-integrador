a9bcf975ebe0b3f25f508838773746adcfd15b87
Esteban <estebancanoc@gmail.com>
2023-05-10 01:51:42 -0500

    Agregada la notacion @Enumerated(EnumType.STRING) Reiniciada la base de datos el tipo de dato cambio.

--- src/main/java/com/esteban/mensajeria/model/Envio.java
+++ src/main/java/com/esteban/mensajeria/model/Envio.java
@@ -1,6 +1,5 @@
 package com.esteban.mensajeria.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +7,6 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @Entity
