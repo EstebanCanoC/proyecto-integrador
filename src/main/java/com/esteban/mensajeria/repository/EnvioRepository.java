62766724eeef005a12d15c3ed0949b0be460176e
Esteban <estebancanoc@gmail.com>
2023-05-09 23:22:22 -0500

    Agregado crearPaquete() en PaqueteService

--- src/main/java/com/esteban/mensajeria/repository/EnvioRepository.java
+++ src/main/java/com/esteban/mensajeria/repository/EnvioRepository.java
@@ -1,2 +1,9 @@
-package com.esteban.mensajeria.repository;public interface EnvioRepository {
+package com.esteban.mensajeria.repository;
+
+import com.esteban.mensajeria.model.Envio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnvioRepository extends JpaRepository<Envio, String> {
 }
--- src/main/java/com/esteban/mensajeria/service/PaqueteService.java
+++ src/main/java/com/esteban/mensajeria/service/PaqueteService.java
@@ -1,4 +1,33 @@
 package com.esteban.mensajeria.service;
 
+import com.esteban.mensajeria.model.Paquete;
+import com.esteban.mensajeria.model.TipoPaquete;
+import com.esteban.mensajeria.repository.PaqueteRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaqueteService {
+    private final PaqueteRepository paqueteRepository;
+
+    public PaqueteService(PaqueteRepository paqueteRepository) {
+        this.paqueteRepository = paqueteRepository;
+    }
+
+    public Paquete crearPaquete(Integer peso, double valorDeclaradoPaquete){
+        TipoPaquete tipo;
+        if (peso < 2) {
+            tipo = TipoPaquete.LIVIANO;
+        } else if (peso < 5) {
+            tipo = TipoPaquete.MEDIANO;
+        } else {
+            tipo = TipoPaquete.GRANDE;
+        }
+
+        Paquete paquete = new Paquete();
+        paquete.setTipoPaquete(tipo);
+        paquete.setPeso(peso);
+        paquete.setValorDeclaradoPaquete(valorDeclaradoPaquete);
+        paqueteRepository.save(paquete);
+        return paquete;
+    }
 }
